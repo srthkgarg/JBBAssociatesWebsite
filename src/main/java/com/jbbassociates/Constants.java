abbd58b714dc09fdc3c89829d8827590a0d9454b
Sarthak Garg <sarthak.garg@evolphin.com>
2019-06-17 17:06:19 +0530

    Changes for start up classes.

---
+++ src/main/java/com/jbbassociates/Constants.java
@@ -0,0 +1,38 @@
+package com.jbbassociates;
+
+/**
+ *
+ */
+class Constants {
+  /**
+   *
+   */
+  static String APP_HOME_ENDPOINT = "/home";
+
+  /**
+   *
+   */
+  static String APP_ADMIN_ENDPOINT = "/admin";
+
+  /**
+   *
+   */
+  static String APP_LOGIN_ENDPOINT = "/login";
+
+  /**
+   *
+   */
+  static String APP_SEARCH_ENDPOINT = "/search";
+
+  /**
+   *
+   */
+  static String ROOT_API_ENDPOINT = Constants.APP_HOME_ENDPOINT;
+
+  /**
+   *
+   */
+  static int SERVER_PORT = 8080;
+
+
+}
--- src/main/webapp/index.jsp
+++
@@ -1,1 +0,0 @@
-<%response.sendRedirect("/html/index.html")%>
