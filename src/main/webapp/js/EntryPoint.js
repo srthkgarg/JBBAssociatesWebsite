45393be76b212f23b186f08f7a7fb6601aeeda30
Sarthak Garg <sarthak.garg@evolphin.com>
2019-06-21 19:06:40 +0530

    Changes in project structure.

--- src/main/webapp/js/EntryPoint.js
+++ src/main/webapp/js/EntryPoint.js
@@ -6,12 +6,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { StyleRoot } from 'radium';
+import {BrowserRouter} from 'react-router-dom';
 
 import RootComponent from './ui/RootComponent';
 
 ReactDOM.render(
   <StyleRoot style={{ height: '100%' }}>
-    <RootComponent />
+    <BrowserRouter>
+      <RootComponent/>
+    </BrowserRouter>
   </StyleRoot>, 
-  document.getElementById('root')
+  document.getElementById('rootDiv')
 );
