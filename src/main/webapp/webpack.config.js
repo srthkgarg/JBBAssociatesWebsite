5dd578cb09576c41e76886dbb5b1bb95c7c9b0f0
Sarthak Garg <sarthak.garg@evolphin.com>
2019-06-24 16:37:20 +0530

    Changes in project structure.

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,33 +1,30 @@
-var webpack = require('webpack');
-var fs = require('fs');
+var webpack = require("webpack");
+var path = require("path");
+
+var DIST_DIR = __dirname;
+var SRC_DIR = __dirname;
 
 module.exports = {
-  devtool: 'source-map',
-  entry: {
-    'bundle': __dirname + "/js/EntryPoint.js"
-  },
+  devtool: "source-map",
+  entry: SRC_DIR + "/js/index.js",
   node: {
     __filename: true
   },
   output: {
-    path: __dirname,
-    filename: "[name].js",
+    path: DIST_DIR + "/js",
+    filename: "bundle.js",
+    //publicPath: "./",
     pathinfo: true
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: "babel-loader",
         query: {
-          /*presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['babel-plugin-transform-decorators-legacy', 'babel-plugin-transform-regenerator']*/
+          presets: ["react", "es2015", "stage-2"]
         }
-      },
-      {
-        test: /\.properties/,
-        loader: 'properties-loader'
       }
     ]
   }
